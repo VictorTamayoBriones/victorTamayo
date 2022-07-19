8e0592606ccc69c954ade51270501f579e0e86dd
Víctor Tamayo <victortamayo509@gmail.com>
2022-07-18 20:30:08 -0500

    remove about section

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -8,7 +8,6 @@ export const AppRoutes = () =>{
         <Routes>
             <Route path="/" element={ <Home/> } />
             <Route path="/projects" element={ <Projects/> } />
-            <Route path="/about" element={ <About/> } />
         </Routes>
     )
 }
